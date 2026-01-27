9320354b3972bf7875efaaa7323d003279cdaf5b
wjchorak <westonjchorak@gmail.com>
2026-01-27 16:23:43 -0700

    fix(eslint) Fixed eslint config issue

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,6 +3,21 @@ import globals from "globals";
 import { defineConfig } from "eslint/config";
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.node, ...globals.jest } },
-  { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
+  {
+    files: ["**/*.{js,mjs,cjs}"],
+    plugins: { js },
+    extends: ["js/recommended"],
+    languageOptions: {
+      globals: {
+        ...globals.node,
+        ...globals.jest,
+      },
+    },
+  },
+  {
+    files: ["**/*.js"],
+    languageOptions: {
+      sourceType: "commonjs",
+    },
+  },
 ]);
