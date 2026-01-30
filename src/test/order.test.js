32e14bb4e4b9311adf11067c03a7e10bda276a60
wjchorak <westonjchorak@gmail.com>
2026-01-29 21:55:47 -0700

    add(test) get menu

--- src/test/order.test.js
+++ src/test/order.test.js
@@ -11,6 +11,20 @@ beforeAll(async () => {
   expectValidJwt(testUserAuthToken);
 });
 
+test('get pizza menu', async () => {
+  const menuRes = await request(app)
+    .get('/api/order/menu');
+
+  expect(menuRes.status).toBe(200);
+  expect(menuRes.body).toBeInstanceOf(Array);
+  expect(menuRes.body.length).toBeGreaterThan(0);
+  expect(menuRes.body[0]).toHaveProperty('id');
+  expect(menuRes.body[0]).toHaveProperty('title');
+  expect(menuRes.body[0]).toHaveProperty('image');
+  expect(menuRes.body[0]).toHaveProperty('price');
+  expect(menuRes.body[0]).toHaveProperty('description');
+});
+
 function expectValidJwt(potentialJwt) {
   expect(potentialJwt).toMatch(/^[a-zA-Z0-9\-_]*\.[a-zA-Z0-9\-_]*\.[a-zA-Z0-9\-_]*$/);
 }
