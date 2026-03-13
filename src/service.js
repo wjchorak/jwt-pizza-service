8e3bdb2bfe1fb3a0486c4f276a785f61e5dd07be
wjchorak <westonjchorak@gmail.com>
2026-03-12 22:39:40 -0600

    add(metrics) add request metrics to service.js

--- src/service.js
+++ src/service.js
@@ -5,6 +5,7 @@ const franchiseRouter = require('./routes/franchiseRouter.js');
 const userRouter = require('./routes/userRouter.js');
 const version = require('./version.json');
 const config = require('./config.js');
+const metrics = require('./metrics');
 
 const app = express();
 app.use(express.json());
@@ -16,6 +17,8 @@ app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Credentials', 'true');
   next();
 });
+
+app.use(metrics.requestTracker);
 
 const apiRouter = express.Router();
 app.use('/api', apiRouter);
